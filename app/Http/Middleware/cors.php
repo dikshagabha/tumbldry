b4f88a60891673f0a411dc7968f8749ed24458d8
dikshagabha <diksha.gabha95@gmail.com>
2019-06-06 20:11:20 +0530

    cors

--- app/Http/Middleware/cors.php
+++ app/Http/Middleware/cors.php
@@ -16,9 +16,9 @@ class cors {
     public function handle($request, Closure $next)
     {
     return $next($request)
-      ->header(‘Access-Control-Allow-Origin’, ‘*’)
-      ->header(‘Access-Control-Allow-Methods’, ‘GET, POST, PUT, DELETE, OPTIONS’)
-      ->header(‘Access-Control-Allow-Headers’, ‘X-Requested-With, Content-Type, X-Token-Auth, Authorization’);
+      ->header('Access-Control-Allow-Origin', '*')
+      ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS')
+      ->header('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type, X-Token-Auth, Authorization');
     }
 
 }
