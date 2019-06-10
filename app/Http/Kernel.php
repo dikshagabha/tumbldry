54d0caec36b3639cb87ab9ce2f5c4cd2ef59545c
EC2 Default User <ec2-user@ip-172-31-24-196.us-east-2.compute.internal>
2019-06-10 09:06:49 +0000

    tasks

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -19,8 +19,8 @@ class Kernel extends HttpKernel
         \App\Http\Middleware\TrimStrings::class,
         \Illuminate\Foundation\Http\Middleware\ConvertEmptyStringsToNull::class,
         \App\Http\Middleware\TrustProxies::class,
-        \Barryvdh\Cors\HandleCors::class,
-        \App\Http\Middleware\PreflightResponse::class,
+        //\Barryvdh\Cors\HandleCors::class,
+	//        \App\Http\Middleware\PreflightResponse::class,
 
     ];
 
