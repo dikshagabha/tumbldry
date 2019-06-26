1bcc567869829b40e8acddc9bc87f84fc7ff5abe
dikshagabha <diksha.gabha95@gmail.com>
2019-06-26 13:11:57 +0530

    tasks

--- resources/views/store/dashboard.blade.php
+++ resources/views/store/dashboard.blade.php
@@ -164,23 +164,23 @@
           donutWidth: 60,
           donutSolid: true,
           startAngle: 270,
-          showLabel: true
+          showLabel: false
         };
 
-        var responsiveOptions = [
-          ['screen and (min-width: 640px)', {
-            chartPadding: 30,
-            labelOffset: 100,
-            labelDirection: 'explode',
-            labelInterpolationFnc: function(value) {
-              return value;
-            }
-          }],
-          ['screen and (min-width: 1024px)', {
-            labelOffset: 80,
-            chartPadding: 20
-          }]
-        ];
+        // var responsiveOptions = [
+        //   ['screen and (min-width: 640px)', {
+        //     chartPadding: 30,
+        //     labelOffset: 100,
+        //     labelDirection: 'explode',
+        //     labelInterpolationFnc: function(value) {
+        //       return value;
+        //     }
+        //   }],
+        //   ['screen and (min-width: 1024px)', {
+        //     labelOffset: 80,
+        //     chartPadding: 20
+        //   }]
+        // ];
         new Chartist.Pie('#chart3', data1, options );
       
     }
